9539a1ed041f001e5e378a3923b0b018dc8841c4
Prakshal Jain <prakshal@buffalo.edu>
2025-01-11 17:55:54 -0800

    V1 Chimera Website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,63 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <main className={styles.heroImage}>
+        <div className={styles.logo}>
+          <Image
+            alt="Chimera Logo"
+            src='/chimera-logo-2.png'
+            width={0}
+            height={0}
+            layout='responsive'
+          />
+        </div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+        <div className={styles.chimera}>
+          <Image
+            alt="Chimera Name"
+            src='/chimera-logo.png'
+            width={0}
+            height={0}
+            layout='responsive'
+          />
+        </div>
+        <h2 className={styles.tagline}>We make the buying experience as exceptional as your car</h2>
+
+        <div className={styles.comparisonView}>
+          <a href="https://youtu.be/2nMCMTNaC5w?si=DsMDX8lr9oFGH8Tm" target="_blank">
+              <Image
+                alt="Chimera Logo"
+                src='/present.png'
+                width={0}
+                height={0}
+                layout='responsive'
+                className={styles.comparisonImage}
+              />
+              <h3 className={styles.comparisonDetail}>
+                How it's done today
+              </h3>
           </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
+
+          <a href="https://youtu.be/LGS8Ls_4rTg?si=QwAF5IR0vGNHiRVp" target="_blank">
+            <Image
+              alt="Chimera Logo"
+              src='/future.png'
+              width={0}
+              height={0}
+              layout='responsive'
+              className={styles.comparisonImage}
+            />
+            <h3 className={styles.comparisonDetail}>
+              How it should be
+            </h3>
           </a>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <div className={styles.content}>
+      </div>
     </div>
   );
 }
