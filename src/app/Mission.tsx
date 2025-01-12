eb2062499e955330086e8e475af74445f86446b2
Prakshal Jain <prakshal@buffalo.edu>
2025-01-11 21:08:08 -0800

    Revamp 1 - prof

---
+++ src/app/Mission.tsx
@@ -0,0 +1,42 @@
+import styles from "./home.module.css";
+import Image from "next/image";
+
+export default function Mission() {
+    return (
+        <div className={styles.content}>
+            <h2>Our Mission</h2>
+            <div>
+                After interviewing over 10+ dealerships and intensive research, we realized that the experience of buying a supercar isn't luxurious and the customers deserves better.
+            </div>
+            <div className={styles.comparisonView}>
+                <a href="https://youtu.be/2nMCMTNaC5w?si=DsMDX8lr9oFGH8Tm" target="_blank">
+                    <Image
+                        alt="Chimera Logo"
+                        src='present.png'
+                        width={0}
+                        height={0}
+                        layout='responsive'
+                        className={styles.comparisonImage}
+                    />
+                    <h3 className={styles.comparisonDetail}>
+                        How it&apos;s done today
+                    </h3>
+                </a>
+
+                <a href="https://youtu.be/LGS8Ls_4rTg?si=QwAF5IR0vGNHiRVp" target="_blank">
+                    <Image
+                        alt="Chimera Logo"
+                        src='future.png'
+                        width={0}
+                        height={0}
+                        layout='responsive'
+                        className={styles.comparisonImage}
+                    />
+                    <h3 className={styles.comparisonDetail}>
+                        How it should be
+                    </h3>
+                </a>
+            </div>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
 import Image from "next/image";
 import styles from "./home.module.css";
+import Mission from "./Mission";
 
 export default function Home() {
   return (
     <div>
       <main className={styles.heroImage}>
-        <div className={styles.logo}>
-          <Image
-            alt="Chimera Logo"
-            src='chimera-logo-2.png'
-            width={0}
-            height={0}
-            layout='responsive'
-          />
-        </div>
-
         <div className={styles.chimera}>
           <Image
             alt="Chimera Name"
@@ -24,41 +15,15 @@ export default function Home() {
             layout='responsive'
           />
         </div>
-        
+
         <h2 className={styles.tagline}>We make the buying experience as exceptional as your car</h2>
 
-        <div className={styles.comparisonView}>
-          <a href="https://youtu.be/2nMCMTNaC5w?si=DsMDX8lr9oFGH8Tm" target="_blank">
-              <Image
-                alt="Chimera Logo"
-                src='present.png'
-                width={0}
-                height={0}
-                layout='responsive'
-                className={styles.comparisonImage}
-              />
-              <h3 className={styles.comparisonDetail}>
-                How it&apos;s done today
-              </h3>
-          </a>
-
-          <a href="https://youtu.be/LGS8Ls_4rTg?si=QwAF5IR0vGNHiRVp" target="_blank">
-            <Image
-              alt="Chimera Logo"
-              src='future.png'
-              width={0}
-              height={0}
-              layout='responsive'
-              className={styles.comparisonImage}
-            />
-            <h3 className={styles.comparisonDetail}>
-              How it should be
-            </h3>
-          </a>
-        </div>
+        <a className={styles.onboardDealershipButton}>
+          Onboard your Dealership
+        </a>
       </main>
-      <div className={styles.content}>
-      </div>
+
+      <Mission />
     </div>
   );
 }
