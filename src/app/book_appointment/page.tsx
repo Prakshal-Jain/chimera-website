7b0d93d1bfdf108a1e49ff7c7b94ecc3e2777dee
Prakshal Jain <prakshal@buffalo.edu>
2025-03-01 16:48:50 -0800

    setup page to get appointment

---
+++ src/app/book_appointment/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function Page() {
+  redirect("https://docs.google.com/forms/d/e/1FAIpQLSdmDb1-gkSrAWy1jwKheFOhVwNyp8p7A7HM7WNKZeEWZX8Vtw/viewform?usp=dialog");
+}
