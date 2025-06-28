991e1176fb1ca528c1cdd511a51a79458034afa2
Prakshal Jain <prakshal@buffalo.edu>
2025-06-28 16:39:44 -0700

    update API url

--- app/variables.tsx
+++ app/variables.tsx
@@ -1,5 +1,5 @@
 const IS_PRODUCTION = true
-const PRODUCTION_API_URL = "https://api.chimeraauto.com"
+const PRODUCTION_API_URL = "https://api.chimeraauto.com/api"
 const DEVELOPMENT_API_URL = "http://localhost:8000"
 
 export const API_URL = IS_PRODUCTION ? PRODUCTION_API_URL : DEVELOPMENT_API_URL
